a206a9b498b8df991af2917e3bdcc59ba9ea49c6
Armianto Sumitro <sumitro@ualberta.ca>
2021-05-05 02:14:59 -0600

    Return list of movies from OMDB

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+
+import { SearchResponse, Movie } from './data/OMDBTypes';
+
 import './App.css';
 
+const API_KEY = 'e287055f';
+
+async function search(title: string): Promise<Movie[]> {
+    let res: SearchResponse = await axios.get('http://www.omdbapi.com', {
+        params: {
+            apikey: API_KEY,
+            s: title,
+        },
+    });
+    return res.data.Search;
+}
+
 function App() {
-    const [search, updateSearch] = useState('');
+    const [title, setTitle] = useState('');
 
     return (
         <div className="App">
@@ -11,13 +27,12 @@ function App() {
                     type="text"
                     id="search"
                     name="search"
-                    value={search}
+                    value={title}
                     placeholder="Search movie title"
-                    onChange={(e) => updateSearch(e.target.value)}
+                    onChange={(e) => setTitle(e.target.value)}
                     onKeyUp={(e) => {
                         if (e.key === 'Enter') {
-                            console.log(search);
-                            // TODO: perform search
+                            search(title);
                         }
                     }}
                 />
