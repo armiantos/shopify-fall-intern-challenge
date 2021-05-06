8fddf01bd612bc266728f5201efd8ff2a31d6950
Armianto Sumitro <sumitro@ualberta.ca>
2021-05-06 00:49:28 -0600

    Fix react HTML elements

--- src/components/Paginator.tsx
+++ src/components/Paginator.tsx
@@ -32,9 +32,9 @@ export function Paginator({
                             aria-hidden="true"
                         >
                             <path
-                                fill-rule="evenodd"
+                                fillRule="evenodd"
                                 d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                                clip-rule="evenodd"
+                                clipRule="evenodd"
                             />
                         </svg>
                     </div>
@@ -44,6 +44,7 @@ export function Paginator({
                     .map((pageNumber) => pageNumber + 1)
                     .map((pageNumber) => (
                         <div
+                            key={pageNumber}
                             className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
                             onClick={(_) => onChangePage(pageNumber)}
                         >
@@ -65,9 +66,9 @@ export function Paginator({
                             aria-hidden="true"
                         >
                             <path
-                                fill-rule="evenodd"
+                                fillRule="evenodd"
                                 d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                clip-rule="evenodd"
+                                clipRule="evenodd"
                             />
                         </svg>
                     </div>
