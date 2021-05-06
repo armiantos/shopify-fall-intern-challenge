c5dfb5f51ae8a9b370221f4de7560e33d64e340b
Armianto Sumitro <sumitro@ualberta.ca>
2021-05-06 01:42:59 -0600

    Refactor Poster to be self contained

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -11,23 +11,29 @@ interface PosterProps {
 }
 
 function Poster({ posterLink: link, alt }: PosterProps) {
+    let posterImage = <img src={link} alt={alt} />;
+
     if (link === 'N/A') {
         // TODO: Replace with svg
-        return <div className="Placeholder bg-gray-300 w-full h-full"></div>;
+        posterImage = (
+            <div className="Placeholder bg-gray-300 w-full h-full"></div>
+        );
     }
 
-    return <img src={link} alt={alt} />;
+    return (
+        <div className="Poster flex-grow grid place-items-center m-4 w-full h-full">
+            {posterImage}
+        </div>
+    );
 }
 
 export function Movie({ movieData }: MovieProps) {
     return (
         <div className="Movie rounded-lg shadow-sm bg-white flex flex-col justify-center items-center p-4">
-            <div className="Poster flex-grow grid place-items-center m-4 w-full h-full">
-                <Poster
-                    posterLink={movieData.Poster}
-                    alt={movieData.Title + '-poster'}
-                />
-            </div>
+            <Poster
+                posterLink={movieData.Poster}
+                alt={movieData.Title + '-poster'}
+            />
             <h3>{movieData.Title}</h3>
             <h4>{movieData.Year}</h4>
         </div>
