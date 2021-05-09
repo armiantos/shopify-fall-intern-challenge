2c708024152dd3011097c3d6b9800573a602f473
Armianto Sumitro <sumitro@ualberta.ca>
2021-05-09 14:28:33 -0600

    Run format on entire project

--- src/App.tsx
+++ src/App.tsx
@@ -19,10 +19,7 @@ const RESULTS_PER_PAGE = 10;
  * @param page with current title to look for. 1-indexed.
  * @returns a SearchResponse (contains a list of titles, posters, and total number of results)
  */
-async function search(
-    title: string,
-    page: number
-): Promise<SearchResponse | undefined> {
+async function search(title: string, page: number): Promise<SearchResponse | undefined> {
     let resp: SearchResponse = await axios.get('http://www.omdbapi.com', {
         params: {
             apikey: API_KEY,
@@ -42,35 +39,22 @@ async function search(
 function App() {
     const [currentPage, setCurrentPage] = useState(1);
     const [nominees, setNominees] = useState<Movie[]>([]);
-    const [lastSearch, setLastSearch] = useState<SearchResponse | undefined>(
-        undefined
-    );
+    const [lastSearch, setLastSearch] = useState<SearchResponse | undefined>(undefined);
 
     return (
         <div className="App">
             <div className="container lg:md mx-auto">
-                <h1 className="text-5xl mt-4 mb-8 font-bold text-green-800">
-                    The Shoppies
-                </h1>
+                <h1 className="text-5xl mt-4 mb-8 font-bold text-green-800">The Shoppies</h1>
 
-                <h2 className="text-4xl text-green-600 mt-4 mb-6 font-medium">
-                    My nominees
-                </h2>
-                {nominees.length === 0 && (
-                    <p>You have not nominated any movies</p>
-                )}
+                <h2 className="text-4xl text-green-600 mt-4 mb-6 font-medium">My nominees</h2>
+                {nominees.length === 0 && <p>You have not nominated any movies</p>}
                 <div className="Nominees grid grid-cols-2 lg:grid-cols-5 gap-4">
                     {nominees.map((movie) => (
                         <MovieComponent key={movie.imdbID} movieData={movie}>
                             <button
                                 className="bg-red-600 hover:bg-red-500 text-white px-4 py-2 m-2 rounded shadow-sm hover:shadow-md transition-all"
                                 onClick={(_) => {
-                                    setNominees(
-                                        nominees.filter(
-                                            (nominee) =>
-                                                nominee.imdbID !== movie.imdbID
-                                        )
-                                    );
+                                    setNominees(nominees.filter((nominee) => nominee.imdbID !== movie.imdbID));
                                 }}
                             >
                                 Remove
@@ -79,9 +63,7 @@ function App() {
                     ))}
                 </div>
 
-                <h2 className="text-4xl text-green-600 mt-4 mb-6 font-medium">
-                    Search
-                </h2>
+                <h2 className="text-4xl text-green-600 mt-4 mb-6 font-medium">Search</h2>
                 <SearchBar
                     onEnter={async (title) => {
                         setLastSearch(await search(title, currentPage));
@@ -92,25 +74,11 @@ function App() {
                         <div className="SearchResults grid grid-cols-2 lg:grid-cols-5 gap-4">
                             {lastSearch.data.Search !== undefined &&
                                 lastSearch.data.Search.map((movie) => (
-                                    <MovieComponent
-                                        key={movie.imdbID}
-                                        movieData={movie}
-                                    >
+                                    <MovieComponent key={movie.imdbID} movieData={movie}>
                                         <button
                                             className="bg-green-600 hover:bg-green-500 text-white px-4 py-2 m-2 rounded shadow-sm hover:shadow-md transition-all disabled:opacity-50 disabled:bg-green-600 disabled:cursor-not-allowed"
-                                            disabled={
-                                                nominees.findIndex(
-                                                    (nominee) =>
-                                                        nominee.imdbID ===
-                                                        movie.imdbID
-                                                ) >= 0
-                                            }
-                                            onClick={(_) =>
-                                                setNominees([
-                                                    ...nominees,
-                                                    movie,
-                                                ])
-                                            }
+                                            disabled={nominees.findIndex((nominee) => nominee.imdbID === movie.imdbID) >= 0}
+                                            onClick={(_) => setNominees([...nominees, movie])}
                                         >
                                             Nominate
                                         </button>
@@ -121,25 +89,14 @@ function App() {
                         <Paginator
                             onChangePage={async (page) => {
                                 setCurrentPage(page);
-                                setLastSearch(
-                                    await search(
-                                        lastSearch.config.params.s,
-                                        page
-                                    )
-                                );
+                                setLastSearch(await search(lastSearch.config.params.s, page));
                             }}
-                            numPages={Math.round(
-                                +lastSearch.data.totalResults / RESULTS_PER_PAGE
-                            )}
+                            numPages={Math.round(+lastSearch.data.totalResults / RESULTS_PER_PAGE)}
                             currentPage={currentPage}
                         />
                     </>
                 )}
-                {lastSearch === undefined && (
-                    <p>
-                        Sorry we could not find any movies with the given title
-                    </p>
-                )}
+                {lastSearch === undefined && <p>Sorry we could not find any movies with the given title</p>}
             </div>
         </div>
     );
--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -10,52 +10,37 @@ interface PosterProps {
     alt: string;
 }
 
+/**
+ * Renders a Movie given its poster link, title, year and optionally additional components to add below it (e.g. a buttton). If the movie poster cannot be found simply enter 'N/A' a placeholder image will be used.
+ *
+ * @param posterLink link to movie poster
+ * @param alt alternative text for accessibility
+ * @returns
+ */
 function Poster({ posterLink: link, alt }: PosterProps) {
     let posterImage = <img className="object-cover" src={link} alt={alt} />;
 
     if (link === 'N/A') {
-        // TODO: Replace with svg
+        // Use placeholder image
         posterImage = (
-            <svg
-                width="100%"
-                viewBox="0 0 648 960"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-            >
+            <svg width="100%" viewBox="0 0 648 960" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <rect width="648" height="960" fill="#E5E5E5" />
                 <rect x="195" y="420" width="257" height="128" fill="#C9C9C9" />
                 <rect x="195" y="389" width="257" height="20" fill="#C9C9C9" />
                 <rect x="209" y="364" width="44" height="21" fill="#C9C9C9" />
-                <circle
-                    cx="351.5"
-                    cy="484.5"
-                    r="43.5"
-                    fill="#C9C9C9"
-                    stroke="#E5E5E5"
-                    strokeWidth="14"
-                />
+                <circle cx="351.5" cy="484.5" r="43.5" fill="#C9C9C9" stroke="#E5E5E5" strokeWidth="14" />
             </svg>
         );
     }
 
-    return (
-        <div className="Poster flex-grow grid place-items-center m-4 w-full h-full aspect-w-11 aspect-h-16">
-            {posterImage}
-        </div>
-    );
+    return <div className="Poster flex-grow grid place-items-center m-4 w-full h-full aspect-w-11 aspect-h-16">{posterImage}</div>;
 }
 
-export const Movie: FunctionComponent<MovieProps> = ({
-    movieData,
-    children,
-}) => {
+export const Movie: FunctionComponent<MovieProps> = ({ movieData, children }) => {
     return (
         <div className="Movie rounded-lg shadow-sm bg-white flex flex-col justify-center items-center p-4">
-            <Poster
-                posterLink={movieData.Poster}
-                alt={movieData.Title + '-poster'}
-            />
-            <h3>{movieData.Title}</h3>
+            <Poster posterLink={movieData.Poster} alt={movieData.Title + '-poster'} />
+            <h3 className="font-medium">{movieData.Title}</h3>
             <h4>{movieData.Year}</h4>
             {children}
         </div>
